e8334ab530515d0c2a4d78ca5b60e97d6a296457
Justin Reynolds <justinreynolds@ou.edu>
2019-02-11 10:47:34 -0600

    Update Todo

--- Todo.java
+++ Todo.java
@@ -1,18 +1,27 @@
 public class Todo {/*
 Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Last Modified_______: 2009-02-11
+OU ID_______________: 112929316
+OU EMAIL____________: justinreynolds@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Create unit tests for the constructors.
+2. Create unit tests for the getters.
+3. Create unit tests for the toString methods.
+4. Edit and improve the unit tests. 
+5. Make commits to github.
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 20
+2. 15
+3. 20
+4. 45
+5. 10
 
 Actual Time (minutes):
 1. XXX
 2. XXX
+3. XXX
+4. XXX
+5. XXX
 */}
