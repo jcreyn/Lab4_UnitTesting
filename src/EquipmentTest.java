6e4d80add3f819d49c035510773e026a5eae5274
Justin Reynolds <justinreynolds@ou.edu>
2019-02-12 20:26:48 -0600

    Create EquipmentTest method. Add fullConstructorTest. Add toStringTest.

--- src/EquipmentTest.java
+++ src/EquipmentTest.java
@@ -1,22 +1,27 @@
 
 public class EquipmentTest {
+
 	
-	//getCapcity test needed
-	// toString test needed
-	
-	
-	 public void emptyConstructorTest() throws AssertException
-	 {
-	        // Use the default constructor
-	        Equipment equipment = new Equipment();
-	        // The name should be "noname", color "unknown", height and weight 0.
-	        Assert.assertEquals("unknown", animal.getColor());
-	        Assert.assertEquals("noname", animal.getName());
-	        Assert.assertEquals(0, animal.getHeight(), 0.01);
-	        Assert.assertEquals(0, animal.getWeight(), 0.01);
-	        }
-	 
-	 
+
+public void fullConstructorTest() throws AssertException
+{
+    Equipment equipment = new Equipment("Chester/4,3.22,22.30,This is description");
+    Assert.assertEquals("Chester", equipment.getName());
+    Assert.assertEquals(4, equipment.getCount());
+    Assert.assertEquals(3.22, equipment.getTotalWeight(), 0.01);
+    Assert.assertEquals(22.30, equipment.getTotalPrice(), 0.01);
+    Assert.assertEquals("This is description", equipment.getDescription());
 }
 
-	 
+public void getColorTest() throws AssertException
+{
+    Animal animal = new Animal();
+    Assert.assertEquals("unknown", animal.getColor());  
+}
+
+public void toStringTest() throws AssertException
+{
+    Equipment equipment = new Equipment("Chester/4,3.22,22.30,This is description");
+    Assert.assertEquals("Name: Chester, Number: 4, Weight: 3.22 lbs, Price: $22.30 - This is description", equipment.toString());
+}
+}
