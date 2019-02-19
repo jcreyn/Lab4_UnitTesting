9d8ecec5d0f78f92caf0791d6eee95406d4f5202
Justin Reynolds <justinreynolds@ou.edu>
2019-02-19 16:27:54 -0600

    Update ZooTest.

--- src/ZooTest.java
+++ src/ZooTest.java
@@ -23,43 +23,30 @@ public class ZooTest {
 
     public void expandZooTest() throws AssertException
     {
-        Zoo zoo = new Zoo(4);
+        Zoo zoo = new Zoo(5);
         zoo.expandZoo();
+        Assert.assertEquals(10, zoo.getCapacity());
         
-        Assert.assertEquals(8, zoo.getCapacity());
         
-        
-        Assert.assertEquals(new Animal[4], zoo.getAnimals());
-        
+        Assert.assertEquals(new Animal[10], zoo.getAnimals());
+
     }
-    
     /**
      * 
      * @throws AssertException
      */
 	
-    public void addAnimalTest() throws AssertException
-    {
-        Zoo zoo = new Zoo(1);
-        Animal animal1 = new Animal("Green", "Chester", 350.0, 40);
-        Animal animal2 = new Animal("Blue", "Chad", 35.0, 40);
-        zoo.addAnimal(animal1);
-        zoo.addAnimal(animal2);
-        Assert.assertEquals(new Animal[]{animal1, animal2, new Animal(), new Animal()}, zoo.getAnimals());
-       // Assert.assertEquals(new Animal[]{animal1, animal2}, newZoo.getAnimals());
-        //Assert.assertEquals(animal1, zoo.getAnimals());
-      
-    }
+   
     
     public void toStringTest() throws AssertException
     {
-    	Zoo zoo = new Zoo(2);
+    	Zoo zoo = new Zoo(1);
         Animal animal1 = new Animal("Green", "Chester", 350.0, 40);
         Animal animal2 = new Animal("Blue", "Chad", 35.0, 40);
         zoo.addAnimal(animal1);
        // zoo.addAnimal(animal2);
         Assert.assertEquals("These animals live in the zoo: \n"
-        		+ new Animal(), zoo.toString());
+        		+ "Chester, a Green-colored animal. 350.0 pounds, 40.0 inches", zoo.toString());
  
     }
 }
